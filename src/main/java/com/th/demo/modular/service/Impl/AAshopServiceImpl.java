2df516a75bdd747d7a2a8c38c0370ba04949c514
865142078@qq.com <865142078@qq.com>
2019-07-02 16:04:34 +0800

    优化

--- src/main/java/com/th/demo/modular/service/Impl/AAshopServiceImpl.java
+++ src/main/java/com/th/demo/modular/service/Impl/AAshopServiceImpl.java
@@ -1,4 +1,4 @@
-package com.th.demo.modular.service.impl;
+package com.th.demo.modular.service.Impl;
 
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.plugins.Page;
@@ -9,7 +9,6 @@ import com.th.demo.common.BussinessException;
 import com.th.demo.common.ResultUtil;
 import com.th.demo.common.SuccessResultEnum;
 import com.th.demo.common.tips.Tip;
-import com.th.demo.modular.dao.AAshopMapper;
 import com.th.demo.modular.entity.AAshop;
 import com.th.demo.modular.service.IAAshopService;
 import org.slf4j.Logger;
@@ -18,6 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -29,14 +32,14 @@ import java.util.stream.Collectors;
  */
 @Service
 @Transactional
-public class AAshopServiceImpl extends ServiceImpl<AAshopMapper,AAshop> implements IAAshopService {
+public class AAshopServiceImpl extends ServiceImpl<com.th.demo.modular.dao.AAshopMapper, AAshop> implements IAAshopService {
     private static final Logger logger = LoggerFactory.getLogger(AAshopServiceImpl.class);
     @Autowired
-    private AAshopMapper AAshopMapper;
+    private com.th.demo.modular.dao.AAshopMapper AAshopMapper;
 
     @Override
-    public Tip getAAList(Page page, String searchvalue,String userToken) throws Exception {
-        if (StringUtils.isEmpty(userToken)){
+    public Tip getAAList(Page page, String searchvalue, String userToken) throws Exception {
+        if (StringUtils.isEmpty(userToken)) {
             return ResultUtil.result(BizExceptionEnum.EMPTY_ERROR.getCode(), BizExceptionEnum.EMPTY_ERROR.getMessage());
         }
         List<AAshop> shop = AAshopMapper.getList(page, searchvalue);
@@ -51,23 +54,25 @@ public class AAshopServiceImpl extends ServiceImpl<AAshopMapper,AAshop> implemen
         //差价
         BigDecimal total = meTotal.subtract(otherTotal);
         JSONObject result = new JSONObject(true);
-        result.put("total",total);
-        result.put("meTotal",meTotal);
-        result.put("otherTotal",otherTotal);
-        result.put("me",meList);
-        result.put("other",otherList);
+        result.put("total", total);
+        result.put("meTotal", meTotal);
+        result.put("otherTotal", otherTotal);
+        result.put("me", meList);
+        result.put("other", otherList);
+        //加日志
+        addLogToTXT(userToken,String.valueOf(total),String.valueOf(meTotal),String.valueOf(otherTotal));
         return ResultUtil.result(SuccessResultEnum.SUCCESS.getCode(), SuccessResultEnum.SUCCESS.getMessage(), result);
     }
 
     @Override
-    public Tip getDetail(Integer id) throws Exception{
+    public Tip getDetail(Integer id) throws Exception {
         AAshop detail = AAshopMapper.selectById(id);
-        return ResultUtil.result(SuccessResultEnum.SUCCESS.getCode(), SuccessResultEnum.SUCCESS.getMessage(), detail==null?"":detail);
+        return ResultUtil.result(SuccessResultEnum.SUCCESS.getCode(), SuccessResultEnum.SUCCESS.getMessage(), detail == null ? "" : detail);
     }
 
     @Override
-    public Tip insertAAshop(AAshop shop) throws Exception{
-        if (StringUtils.isEmpty(shop.getUserToken())){
+    public Tip insertAAshop(AAshop shop) throws Exception {
+        if (StringUtils.isEmpty(shop.getUserToken())) {
             return ResultUtil.result(BizExceptionEnum.EMPTY_ERROR.getCode(), BizExceptionEnum.EMPTY_ERROR.getMessage());
         }
         SimpleDateFormat smf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -77,9 +82,9 @@ public class AAshopServiceImpl extends ServiceImpl<AAshopMapper,AAshop> implemen
     }
 
     @Override
-    public Tip login(String userName, String passWord) throws Exception{
-        Integer count = AAshopMapper.login(userName,passWord);
-        if (count == 0){
+    public Tip login(String userName, String passWord) throws Exception {
+        Integer count = AAshopMapper.login(userName, passWord);
+        if (count == 0) {
             throw new BussinessException(BizExceptionEnum.FAIL_LOGIN);
         }
         return ResultUtil.result(SuccessResultEnum.SUCCESS.getCode(), SuccessResultEnum.SUCCESS.getMessage());
@@ -92,6 +97,20 @@ public class AAshopServiceImpl extends ServiceImpl<AAshopMapper,AAshop> implemen
     public Tip account() {
         AAshopMapper.account();
         return ResultUtil.result(SuccessResultEnum.SUCCESS.getCode(), SuccessResultEnum.SUCCESS.getMessage());
+    }
+
+    private static void addLogToTXT(String userToken,String total,String meTotal,String otherTotal) throws Exception {
+        File writename = new File("aalog.txt");
+        if(!writename.exists()){
+            writename.createNewFile(); // 创建新文件
+        }
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(writename, true)));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String now = sdf.format(new Date());
+        out.write(now+"：用户*"+userToken+"*——所用金额："+meTotal+"元,他人所用金额："+otherTotal+ "元，累计差价为："+total+"元\r\n"); // \r\n即为换行
+        out.flush(); // 把缓存区内容压入文件
+        out.close(); // 最后记得关闭文件
     }
 
 }
